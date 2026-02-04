22ce1c93264d7179f25d8e5d622fadc8c656fab0
veggiemania <milkcowgames@gmail.com>
2026-02-04 19:08:15 +0100

    [BackButton] The code has been divided into #region sections and comments have been added to each method

--- Assets/Scripts/Game/BackButton.cs
+++ Assets/Scripts/Game/BackButton.cs
@@ -3,11 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class BackButton : MonoBehaviour
 {
-    [SerializeField]
-    private string targetSceneName = "Menu";
+    #region Serialized Fields
 
+    [Header("Scene Navigation")]
+    [SerializeField] private string targetSceneName = "Menu";
+
+    #endregion
+
+    #region Public Methods
+
+    /// <summary>
+    /// Navigates back to the target scene (typically the main menu).
+    /// Called when the back button is clicked.
+    /// </summary>
     public void GoBack()
     {
+        // Load the target scene
         SceneManager.LoadScene(targetSceneName);
     }
+
+    #endregion
 }
